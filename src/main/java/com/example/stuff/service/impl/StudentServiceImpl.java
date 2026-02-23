329e600db6b800ce480093cb0a97707eddac86d5
Y3llow45 <hristovborislav45@gmail.com>
2026-02-23 15:17:17 +0200

    Update StudentServiceImpl.java

--- src/main/java/com/example/stuff/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/stuff/service/impl/StudentServiceImpl.java
@@ -4,15 +4,20 @@ import com.example.stuff.entity.Student;
 import com.example.stuff.repository.StudentRepository;
 import com.example.stuff.service.StudentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor
 public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
     @Override
     public List<Student> getAllStudentsWithAgeGreaterThan(int age) {
